3aa704fadc16ab27b07e7433bda18dfa8e3a4aaf
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-25 18:24:35 +0530

    Update climate.py

--- ULTRA/plugins/climate.py
+++ ULTRA/plugins/climate.py
@@ -13,8 +13,8 @@ import requests
 from pytz import country_names as c_n
 from pytz import country_timezones as c_tz
 from pytz import timezone as tz
-from userbot import CMD_HELP
-from userbot.utils import admin_cmd, edit_or_reply, errors_handler, sudo_cmd
+from ULTRA import CMD_HELP
+from ULTRA.utils import admin_cmd, edit_or_reply, errors_handler, sudo_cmd
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
