38c9d4bd66393cea8111a5b7f81a879e0b1a5fd3
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-25 16:19:21 +0530

    Update calc.py

--- ULTRA/plugins/calc.py
+++ ULTRA/plugins/calc.py
@@ -6,8 +6,8 @@ import io
 import sys
 import traceback
 
-from userbot import CMD_HELP
-from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from ULTRA import CMD_HELP
+from ULTRA.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @bot.on(admin_cmd(pattern="calc (.*)"))
