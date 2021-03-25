9f0115fad4bad4982ae60efd62a0dad40758b0a0
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-25 19:55:16 +0530

    Update LEGENDX.py

--- LEGENDX.py
+++ LEGENDX.py
@@ -1,9 +1,5 @@
 # COPYRIGHT (C) 2021 BY LEGENDX22
 """
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
 """
 # MADE BY LEGENDX22 🔥
 # MY IDEA H YRR DONT KANG THIS PLEASE
@@ -24,14 +20,14 @@ from telethon import events, custom, Button, TelegramClient
 import time
 from userbot import botnickname, ALIVE_NAME, bot
 pbot = Client("LEGEND", api_id=API_ID, api_hash=API_HASH, bot_token=token)
-BOT = str(botnickname) if botnickname else "UltraX"
-NAME = str(ALIVE_NAME) if ALIVE_NAME else "UltraX User"
+BOT = str(botnickname) if botnickname else "υℓтяα χ"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
-LEGENDX = "[LEGEND X](https://t.me/LEGENDX22)"
+ULTRAX = "[ULTRA X](https://t.me/ULTRAXOT)"
 VERSION = "3.1.5"
-devs = [1100231654, 1636374066] # , id, id, id aise krke full krde
+devs = [1100231654, 1636374066,1037581197] # , id, id, id aise krke full krde
 ID = 1100231654
-REPO = "[LEGEND BOT](https://github.com/ULTRA-OP/ULTRA-X)"
+REPO = "[υℓтяα χ вσт](https://github.com/ULTRA-OP/ULTRA-X)"
 PRO = bot.uid
 MASTER = f"[{NAME}](tg://user?id={PRO})"
 GROUP = "[SUPPORT GROUP](https://t.me/UltraXChat)"
