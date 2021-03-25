3c490d0b9f14f6c839d769d1558cd870f1b4d452
PROBOY X <68194873+PROBOYX@users.noreply.github.com>
2021-03-25 10:22:02 +0530

    Hmm

---
+++ ULTRA/plugins/inline.py
@@ -0,0 +1,13 @@
+from telethon import Button, custom, events
+import os, re, sys, asyncio
+@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b'restart')))
+async def restart(event):
+  if event.sender_id == bot.me.id:
+    await event.edit("restarting your bot please wait")
+    await asyncio.sleep(2)
+    await event.edit("restarted your bot succesfully")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit ()
+  else:
+    pro = "DEPLOY YOUR OWN ULTRA X PLEASE"
+    await event.answer(pro, alert=True)
