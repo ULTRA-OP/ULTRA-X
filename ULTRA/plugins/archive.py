117bf768fed0d4c16431068223d25b079a6ae022
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-25 15:26:50 +0530

    Update archive.py

--- ULTRA/plugins/archive.py
+++ ULTRA/plugins/archive.py
@@ -16,13 +16,13 @@ import shutil
 import subprocess
 import time
 from pySmartDL import SmartDL
-from userbot.uniborgConfig import Config
+from ULTRA.uniborgConfig import Config
 from telethon import events
-from userbot.utils import admin_cmd, humanbytes, progress, time_formatter
+from ULTRA.utils import admin_cmd, humanbytes, progress, time_formatter
 import subprocess
 import patoolib
 import tarfile
-from userbot import CMD_HELP
+from ULTRA import CMD_HELP
 
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 extracted = Config.TMP_DOWNLOAD_DIRECTORY + "extracted/"
