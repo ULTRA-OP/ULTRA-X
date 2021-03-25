1063758f8cbb341ac38bc874f5f8b5d14150350a
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-25 18:47:47 +0530

    Update execmod.py

--- ULTRA/plugins/execmod.py
+++ ULTRA/plugins/execmod.py
@@ -11,7 +11,7 @@ import time
 import os
 import sys
 from telethon import events, functions, __version__
-from userbot.utils import admin_cmd
+from ULTRA.utils import admin_cmd
 from asyncio.subprocess import PIPE as asyncPIPE
 from asyncio import create_subprocess_exec as asyncrunapp
 
