de4e75d984bdc68b373c6587df2463a8b3773119
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-25 11:07:56 +0530

    Update __main__.py

--- ULTRA/__main__.py
+++ ULTRA/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("LEGEND BOT is on fire 🔥🔥🔥 all files installed on your bot.... Join @teamishere for any help..")
+print("Ultra-X is Up and Awake! ©️TeamLegend 2021")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
