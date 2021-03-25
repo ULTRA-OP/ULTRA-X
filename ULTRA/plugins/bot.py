e48a3d1f3b4dae4699d0727227019e205146cfce
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-25 15:17:01 +0530

    Update bot.py

--- ULTRA/plugins/bot.py
+++ ULTRA/plugins/bot.py
@@ -11,21 +11,21 @@ from io import BytesIO
 from datetime import datetime
 import random
 from telethon import events, Button, custom
-from userbot.utils import admin_cmd
-from userbot import ALIVE_NAME
-from userbot import bot as borg
+from ULTRA.utils import admin_cmd
+from ULTRA import ALIVE_NAME
+from ULTRA import bot as ultra
 from telethon.tl.custom import Button, custom
 from telethon.tl.types import ChannelParticipantsAdmins
 global ok
-ok = borg.uid
-from userbot.utils import admin_cmd
+ok = ultra.uid
+from ULTRA.utils import admin_cmd
 from PIL import Image
 import requests
 from io import BytesIO
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND BOY"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
 ALIVE_PHOTTO = PHOTO
 
-pro_text=(f"**{BOT} IS ON FIRE **\n\n**Yes Master, Am Alive And Systems Are Working Perfectly As It Should Be...**\n\n🔥 About My System 🔥\n\n➾ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ** ☞ 1.20\n➾ **ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ** ☞ [ᴊᴏɪɴ](https://t.me/LEGENDBOT_OFFICIAL)\n➾ **ʟɪᴄᴇɴꜱᴇ**  ☞ [LEGEND BOT](https://github.com/legendx22)\n➾ **ᴄᴏᴘʏʀɪɢʜᴛ ʙʏ** ☞ [LEGEND-BOT](https://github.com/legendx22/LEGEND-BOT)\n\n➾ **ᴍʏ ᴍᴀsᴛᴇʀ** ☞ [{DEFAULTUSER}](tg://user?id={ok})\n")
+pro_text=(f"**{BOT} ιѕ ση ƒιяє**\n\n**уєѕ мαѕтєя, αм αℓινє αη∂ ѕуѕтємѕ αяє ωσякιηg ρєяƒє¢тℓу αѕ ιт ѕнσυℓ∂ вє...**\n\n🔥 αвσυт му ѕуѕтєм 🔥\n\n➾ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ** ☞ 1.19.5\n➾ **ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ** ☞ [ᴊᴏɪɴ](https://t.me/ULTRAXOT)\n➾ **ʟɪᴄᴇɴꜱᴇ**  ☞ [υℓтяα χ](https://github.com/ULTRA-OP)\n➾ **ᴄᴏᴘʏʀɪɢʜᴛ ʙʏ** ☞ [υℓтяα χ вσт](https://github.com/ULTRA-OP/ULTRA-X)\n\n➾ **ᴍʏ ᴍᴀsᴛᴇʀ** ☞ [{DEFAULTUSER}](tg://user?id={ok})\n")
 TG_BOT_USER_NAME_BF_HER = os.environ.get("ALIVE_PHOTTO", None)
 if TG_BOT_USER_NAME_BF_HER is not None:
     @tgbot.on(events.InlineQuery)
@@ -33,14 +33,14 @@ if TG_BOT_USER_NAME_BF_HER is not None:
         builder = event.builder
         result = None
         query = event.text
-        me = await borg.get_me()
+        me = await ultra.get_me()
         if query.startswith("alive") and event.query.user_id == me.id:
             buttons = [
                 [
-                    Button.url("Repo", "https://github.com/LEGENDXOP/LEGEND-BOT"),
-                    Button.url("Deploy", "https://heroku.com/deploy?template=https://github.com/legendx22/LEGEND-BOT/blob/master")],
+                    Button.url("Repo", "https://github.com/ULTRA-OP/ULTRA-X"),
+                    Button.url("Deploy", "https://heroku.com/deploy?template=https://github.com/ULTRA-OP/ULTRA-X/blob/master")],
                     [Button.url("String", "https://repl.it/legendx22/LEGEND-BOT#main.py"),
-                    Button.url("Channel", "https://t.me/teamishere")
+                    Button.url("Channel", "https://t.me/ULTRAXOT")
                 ]
             ]
              buttons += [[custom.Button.inline("HELP", data="helpme"), custom.Button.inline("restart", data='restart')]]
@@ -54,14 +54,14 @@ if TG_BOT_USER_NAME_BF_HER is not None:
             elif ALIVE_PHOTTO:
                 result = builder.document(
                     ALIVE_PHOTTO,
-                    title="LEGEND BOT",
+                    title="υℓтяα χ",
                     text=pro_text,
                     buttons=buttons,
                     link_preview=False,
                 )
             else:
                 result = builder.article(
-                    title="LEGEND BOT",
+                    title="υℓтяα χ",
                     text=pro_text,
                     buttons=buttons,
                     link_preview=False,
@@ -70,16 +70,16 @@ if TG_BOT_USER_NAME_BF_HER is not None:
 
 
 
-from userbot import bot
+from ULTRA import bot 
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="alive"))
 async def repo(event):
     if event.fwd_from:
         return
-    LEGENDX = Var.TG_BOT_USER_NAME_BF_HER
+    ULTRAX = Var.TG_BOT_USER_NAME_BF_HER
     if event.reply_to_msg_id:
         await event.get_reply_message()
-    response = await bot.inline_query(LEGENDX, "alive")
+    response = await bot.inline_query(ULTRAX, "alive")
     await response[0].click(event.chat_id)
     await event.delete()
