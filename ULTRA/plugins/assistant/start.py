d94b35efad9776a89883b2b2ef188fb012a3c55b
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-25 12:40:48 +0530

    Update start.py

--- ULTRA/plugins/assistant/start.py
+++ ULTRA/plugins/assistant/start.py
@@ -1,5 +1,5 @@
 from ULTRA import tbot, bot
-from telethon import events
+from telethon import events, Button
 
 @tbot.on(events.NewMessage(pattern="^/start"))
 async def startass(e):
@@ -9,4 +9,9 @@ async def startass(e):
   if e.sender_id = k:
    await e.reply('Hello Master I am Alive and Awake')
   else:
-   await e.reply('Hello! How can I help Ya')
+   buttons=[[
+                    Button.url("Deploy Ultra-X", "https://github.com/Ultra-OP/Ultra-X"),
+                    Button.url("Visit Support", "t.me/UltraXChat"),
+                ]]
+   caption = "Hello Im Ultra-X Bot Assistant, Deploy Your Own Ultra-X Bot to Unleash My Full Potential"
+   await tbot.send_message(e.chat_id, caption, buttons=buttons)
