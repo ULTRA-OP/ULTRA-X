dd4a09b40903684bcaaf55398a5c261587671c39
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-03-25 19:34:56 +0530

    Update LEGENDX.py

--- LEGENDX.py
+++ LEGENDX.py
@@ -16,24 +16,24 @@ except:
   from pyrogram import Client, idle
 
 import asyncio
-from userbot.utils import admin_cmd as legendx
-from userbot import bot as LEGENDX22
+from ULTRA.utils import admin_cmd as legendx
+from ULTRA import bot as LEGENDX22
 API_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 from telethon import events, custom, Button, TelegramClient
 import time
 from userbot import botnickname, ALIVE_NAME, bot
 pbot = Client("LEGEND", api_id=API_ID, api_hash=API_HASH, bot_token=token)
-BOT = str(botnickname) if botnickname else "LEGEND BOT"
-NAME = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND BOY"
+BOT = str(botnickname) if botnickname else "UltraX"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "UltraX User"
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
 LEGENDX = "[LEGEND X](https://t.me/LEGENDX22)"
 VERSION = "3.1.5"
 ID = 1100231654
-REPO = "[LEGEND BOT](https://github.com/LEGENDXOP/LEGEND-BOT)"
+REPO = "[LEGEND BOT](https://github.com/ULTRA-OP/ULTRA-X)"
 PRO = bot.uid
 MASTER = f"[{NAME}](tg://user?id={PRO})"
-GROUP = "[SUPPORT GROUP](https://t.me/LEGEND_USERBOT_SUPPORT)"
+GROUP = "[SUPPORT GROUP](https://t.me/UltraXChat)"
 if __name__=="__main__":
   xbot.run_until_disconnected()
 
