277b5a9f77e6f684b2f29066401d7402312a7573
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-26 11:09:26 +0530

    Update get_bot.py

--- ULTRA/plugins/get_bot.py
+++ ULTRA/plugins/get_bot.py
@@ -2,8 +2,8 @@
 Syntax: .get_bot"""
 from telethon import events
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantsBots
-from userbot import CMD_HELP
-from userbot.utils import admin_cmd
+from ULTRA import CMD_HELP
+from ULTRA.utils import admin_cmd
 
 
 @borg.on(admin_cmd("get_bot ?(.*)"))
