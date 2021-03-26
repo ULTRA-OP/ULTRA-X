3a85d8aebf6dc5d8e3cd086f8a5ac56461fe1860
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-26 15:32:52 +0530

    Update plugin_info.py

--- ULTRA/plugins/plugin_info.py
+++ ULTRA/plugins/plugin_info.py
@@ -5,8 +5,8 @@
 #
 """ Userbot plugin_info command """
 
-from userbot import CMD_HELP
-from userbot.utils import admin_cmd
+from ULTRA import CMD_HELP
+from ULTRA.utils import admin_cmd
 
 @borg.on(admin_cmd(outgoing=True, pattern="plinfo(?: |$)(.*)"))
 async def info(event):
