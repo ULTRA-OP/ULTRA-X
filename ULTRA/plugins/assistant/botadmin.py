708decc55b38bc6e587125b8175ba7bd9cd247d2
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-26 17:28:37 +0530

    Update botadmin.py

--- ULTRA/plugins/assistant/botadmin.py
+++ ULTRA/plugins/assistant/botadmin.py
@@ -14,7 +14,7 @@ PP_TOO_SMOL = "`The image is too small`"
 PP_ERROR = "`Failure while processing the image`"
 NO_ADMIN = "`I am not an admin nub nibba!`"
 NO_PERM = (
-    "`I don't have sufficient permissions! This is so sed. Promote Me!`"
+    "`I don't have sufficient permissions! This is so sed. Pls Promote Me!`"
 )
 NO_SQL = "`Running on Non-SQL mode!`"
 
@@ -51,6 +51,42 @@ UNBAN_RIGHTS = ChatBannedRights(
 
 MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
 UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
+
+async def get_user_from_event(event):
+    """ Get the user from argument or replied message. """
+    args = event.pattern_match.group(1).split(" ", 1)
+    extra = None
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_obj = await event.client.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif args:
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+
+        if user.isnumeric():
+            user = int(user)
+
+        if not user:
+            await event.reply("`Pass the user's username, id or reply!`")
+            return
+
+        if event.message.entities is not None:
+            probable_user_mention_entity = event.message.entities[0]
+
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        try:
+            user_obj = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+
+    return user_obj, extra
+
 
 @tbot.on(events.NewMessage(pattern="/ban ?(.*)"))
 async def ban(event):
