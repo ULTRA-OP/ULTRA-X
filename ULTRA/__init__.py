9a465f0231e71746b8dc377eb96645e5fd257290
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-27 11:49:14 +0530

    Update __init__.py

--- ULTRA/__init__.py
+++ ULTRA/__init__.py
@@ -16,7 +16,6 @@ if Var.STRING_SESSION:
 else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
-from LEGENDX import token
 tbot = TelegramClient(None, Var.API_KEY, Var.API_HASH)
 DEVS = [1100231654, 1636374066, 1037581197, 1695676469, 1221693726, 1207066133, 1513257955]
 
