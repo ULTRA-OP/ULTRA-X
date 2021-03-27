a06203d2a24f4565e0ed3d1a2beaef3544f7cd9c
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-27 09:46:41 +0530

    Update instant_install_ext_module.py

--- ULTRA/plugins/instant_install_ext_module.py
+++ ULTRA/plugins/instant_install_ext_module.py
@@ -1,30 +1,18 @@
-#   Copyright 2019 - 2020 DarkPrinc3
 
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
 
-#       http://www.apache.org/licenses/LICENSE-2.0
-
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
-from userbot import bot
+from ULTRA import bot
 from telethon import events
 from telethon import functions, types
 from telethon.tl.types import InputMessagesFilterDocument
-from userbot.utils import command, remove_plugin, load_module
+from ULTRA.utils import command, remove_plugin, load_module
 from var import Var
 from pathlib import Path
-from userbot import LOAD_PLUG
+from ULTRA import LOAD_PLUG
 import sys
 import asyncio
 import traceback
 import os
-import userbot.utils
+import ULTRA.utils
 
 @command(pattern="^.extdl", outgoing=True)
 async def install(event):
