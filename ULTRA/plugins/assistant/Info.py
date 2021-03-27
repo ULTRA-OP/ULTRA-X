163c89f0e91c1f835ce5e76ec8bba77d3f47a72a
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-27 20:26:49 +0530

    Update Info.py

--- ULTRA/plugins/assistant/Info.py
+++ ULTRA/plugins/assistant/Info.py
@@ -1,5 +1,5 @@
 from telethon.utils import pack_bot_file_id
-from ULTRA import tbot
+from ULTRA import tbot, bot
 from telethon import events
 
 @tbot.on(events.NewMessage(pattern="/id ?(.*)"))
@@ -26,3 +26,86 @@ async def _(event):
         await tbot.send_message(
             event.chat_id, "Current Chat ID: `{}`".format(str(event.chat_id))
         )
+
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+import os
+
+pro = await bot.get_me()
+boy = pro.id
+
+@tbot.on(events.NewMessage(pattern="/info ?(.*)"))
+async def who(event):
+    replied_user = await get_user(event)
+    try:
+        caption = await detail(replied_user, event)
+    except AttributeError:
+        event.edit("`Could not fetch info of that user.`")
+        return
+    message_id_to_reply = event.message.reply_to_msg_id
+    if not message_id_to_reply:
+        message_id_to_reply = None
+    await event.reply(caption, parse_mode="html")
+
+
+async def get_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        replied_user = await tbot(GetFullUserRequest(previous_message.sender_id))
+    else:
+        user = event.pattern_match.group(1)
+
+        if user.isnumeric():
+            user = int(user)
+
+        if not user:
+            self_user = await event.get_sender()
+            user = self_user.id
+
+        if event.message.entities is not None:
+            probable_user_mention_entity = event.message.entities[0]
+
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await tbot(GetFullUserRequest(user_id))
+                return replied_user
+        try:
+            user_object = await tbot.get_entity(user)
+            replied_user = await tbot(GetFullUserRequest(user_object.id))
+        except (TypeError, ValueError) as err:
+            await event.reply("I don't seem to have interacted with this user before - please forward a message from them to give me control! (like a voodoo doll, I need a piece of them to be able to execute certain commands...)")
+            return None
+
+    return replied_user
+
+async def detail(replied_user, event):
+ try:
+    user_id = replied_user.user.id
+    first_name = replied_user.user.first_name
+    last_name = replied_user.user.last_name
+    username = replied_user.user.username
+    first_name = (
+        first_name.replace("\u2060", "")
+    )
+    last_name = (
+        last_name.replace("\u2060", "") if last_name else None
+    )
+    username = "@{}".format(username) if username else None
+
+    caption = "<b>User Info:</b> \n"
+    caption += f"ID: <code>{user_id}</code> \n"
+    caption += f"First Name: {first_name} \n"
+    if last_name:
+      caption += f"Last Name: {last_name} \n"
+    if username:
+      caption += f"Username: {username} \n"
+    caption += f'User link: <a href="tg://user?id={user_id}">link</a>'
+    if not event.sender_id == boy:
+       if user_id == boy:
+        caption += "\n<b>This is My Master Beware!</b>"
+    else:
+        caption += "\n<b>Hello Master☺️</b>"
+    return caption
+ except Exception:
+        print("lel")
+
