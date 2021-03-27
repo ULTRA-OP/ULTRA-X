e045adda0114c68d58011e48cbbbdac8c0818f87
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-27 21:40:12 +0530

    Fixed 🙂

--- LEGENDX.py
+++ LEGENDX.py
@@ -6,11 +6,13 @@
 import asyncio
 import os
 import asyncio
+from telethon import TelegramClient
 from ULTRA.utils import admin_cmd as legendx
 from ULTRA import bot as LEGENDX22
 API_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
-from telethon import events, custom, Button, TelegramClient
+token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
+xbot = TelegramClient("legend", API_ID, API_HASH).start(bot_token=token)
 import time
 from ULTRA import botnickname, ALIVE_NAME, bot
 BOT = str(botnickname) if botnickname else "υℓтяα χ"
@@ -24,3 +26,5 @@ REPO = "[υℓтяα χ вσт](https://github.com/ULTRA-OP/ULTRA-X)"
 PRO = bot.uid
 MASTER = f"[{NAME}](tg://user?id={PRO})"
 GROUP = "[SUPPORT GROUP](https://t.me/UltraXChat)"
+if __name__=="__main__":
+  xbot.run_until_disconnected()
