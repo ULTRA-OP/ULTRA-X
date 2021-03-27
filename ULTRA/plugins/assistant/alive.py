9d6fcd1bb90ef8bd73a5b4e3438f07a45d9e0102
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-27 21:18:10 +0530

    Update alive.py

--- ULTRA/plugins/assistant/alive.py
+++ ULTRA/plugins/assistant/alive.py
@@ -1,6 +1,21 @@
 from ULTRA import tbot
 from telethon import events
+from ULTRA import StartTime
+from ULTRA.events import get_readable_time
+import time
+import datetime
 
-@tbot.on(events.NewMessage(pattern="/alive"))
+@tbot.on(events.NewMessage(pattern="/ping"))
 async def ok(event):
-   await event.reply('Not Done')
+    start_time = datetime.datetime.now()
+    message = await event.reply("Pinging_")
+    end_time = datetime.datetime.now()
+    pingtime = end_time - start_time
+    telegram_ping = str(round(pingtime.total_seconds(), 2)) + "s"
+    uptime = get_readable_time((time.time() - StartTime))
+    await message.edit(
+        "PONG !\n"
+        "<b>Time Taken:</b> <code>{}</code>\n"
+        "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        parse_mode="html",
+    )
