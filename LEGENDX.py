b380d11c14bcdef3bd8f2504cf0e6c3efe4faec7
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-27 19:29:34 +0530

    Update LEGENDX.py

--- LEGENDX.py
+++ LEGENDX.py
@@ -5,12 +5,6 @@
 # MY IDEA H YRR DONT KANG THIS PLEASE
 import asyncio
 import os
-try:
-  from pyrogram import Client, idle
-except:
-  os.system("pip install pyrogram>=1.1.13")
-  from pyrogram import Client, idle
-
 import asyncio
 from ULTRA.utils import admin_cmd as legendx
 from ULTRA import bot as LEGENDX22
@@ -19,7 +13,6 @@ API_HASH = os.environ.get("API_HASH", None)
 from telethon import events, custom, Button, TelegramClient
 import time
 from ULTRA import botnickname, ALIVE_NAME, bot
-pbot = Client("LEGEND", api_id=API_ID, api_hash=API_HASH, bot_token=token)
 BOT = str(botnickname) if botnickname else "υℓтяα χ"
 NAME = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
@@ -31,9 +24,3 @@ REPO = "[υℓтяα χ вσт](https://github.com/ULTRA-OP/ULTRA-X)"
 PRO = bot.uid
 MASTER = f"[{NAME}](tg://user?id={PRO})"
 GROUP = "[SUPPORT GROUP](https://t.me/UltraXChat)"
-if __name__=="__main__":
-  xbot.run_until_disconnected()
-
-if __name__=="__main__":
-  pbot.start()
-  run()
