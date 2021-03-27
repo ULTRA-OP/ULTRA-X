84e432aa2e1ef24f252e588fb44a912eafc6c5dc
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-27 11:46:32 +0530

    Update __main__.py

--- ULTRA/__main__.py
+++ ULTRA/__main__.py
@@ -1,4 +1,12 @@
-from ULTRA import bot
+from telethon.sessions import StringSession
+from telethon import TelegramClient
+from var import Var
+if Var.STRING_SESSION:
+    session_name = str(Var.STRING_SESSION)
+    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 from sys import argv
 import sys
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
