78dbe1365b74bf530a9c4473a63e5809933ba924
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-27 16:43:39 +0530

    Create Info.py

---
+++ ULTRA/plugins/assistant/Info.py
@@ -0,0 +1,27 @@
+from telethon.utils import pack_bot_file_id
+from ULTRA import tbot
+
+@tbot.on(events.NewMessage(pattern="/id ?(.*)"))
+async def _(event):
+    if event.reply_to_msg_id:
+        await event.get_input_chat()
+        r_msg = await event.get_reply_message()
+        if r_msg.media:
+            bot_api_file_id = pack_bot_file_id(r_msg.media)
+            await tbot.send_message(
+                event.chat_id,
+                "Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(
+                    str(event.chat_id), str(r_msg.sender_id), bot_api_file_id
+                ),
+            )
+        else:
+            await tbot.send_message(
+                event.chat_id,
+                "Current Chat ID: `{}`\nFrom User ID: `{}`".format(
+                    str(event.chat_id), str(r_msg.sender_id)
+                ),
+            )
+    else:
+        await tbot.send_message(
+            event.chat_id, "Current Chat ID: `{}`".format(str(event.chat_id))
+        )
