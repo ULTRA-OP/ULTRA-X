adb6217b6ed2cf050f82c9dd244d94b2c599846d
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-03-27 11:29:20 +0530

    Update session.py

--- session.py
+++ session.py
@@ -17,7 +17,7 @@ while True:
    session = client.session.save()
    client.send_message(
        "me",
-       f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @UltraXoT For Any Help !"
+       f"Here Is Your String Session For Using UltraX Userbot\n(**Tap to copy it**)👇 \n\n `{session}` \n\n And Visit @UltraXOT For Any Help !"
    )
 
    print(
