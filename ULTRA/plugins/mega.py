81e2b6d59b85de1f4c1efc4ac10b438306ecb3f5
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-27 09:29:12 +0530

    Update mega.py

--- ULTRA/plugins/mega.py
+++ ULTRA/plugins/mega.py
@@ -1,22 +1,4 @@
-# Copyright (C) 2020 Adek Maulana.
-# All rights reserved.
-#
-# Redistribution and use of this script, with or without modification, is
-# permitted provided that the following conditions are met:
-#
-# 1. Redistributions of this script must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#
-#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
-#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
-#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 import asyncio
 import errno
@@ -32,8 +14,8 @@ from urllib.error import HTTPError
 
 from pySmartDL import SmartDL
 
-from userbot.utils import admin_cmd, edit_or_reply, humanbytes, time_formatter
-from userbot import CMD_HELP, LOGS
+from ULTRA.utils import admin_cmd, edit_or_reply, humanbytes, time_formatter
+from ULTRA import CMD_HELP, LOGS
 
 TEMP_DOWNLOAD_DIRECTORY = Config.TMP_DOWNLOAD_DIRECTORY
 
