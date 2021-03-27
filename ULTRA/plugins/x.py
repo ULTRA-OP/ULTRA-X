8cbd294adb2f8f3624f3faef9ec4b414f322c508
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-03-27 10:39:03 +0530

    Update x.py

--- ULTRA/plugins/x.py
+++ ULTRA/plugins/x.py
@@ -35,7 +35,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             if event.query.user_id == bot.uid or event.query.user_id == id:
                 current_page_number=0
                 dc = paginate_help(current_page_number, CMD_LIST, "helpme")
-                await event.edit("`>>>\n\nReopened The Main Menu of \n©LEGENDBOT` ", buttons=dc)
+                await event.edit("`>>>\n\nRᴇᴏᴘᴇɴᴇᴅ Mᴀɪɴ Mᴇɴᴜ Oғ \n© UltraX` ", buttons=dc)
             else:
                 reply_pop_up_alert = "Please get your own Userbot,for more info visit @teamishere!"
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
