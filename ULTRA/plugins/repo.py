c04eff4363479222c0f947ee851420ea49115dcf
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-28 12:02:19 +0530

    Done

--- ULTRA/plugins/repo.py
+++ ULTRA/plugins/repo.py
@@ -1,20 +1,18 @@
 
-
-Betichod = "RoseLoverX"
 from telethon import events, Button, custom
-from ULTRA import bot, xbot
+from ULTRA import bot
+from LEGENDX import xbot
 import os,re
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
 @xbot.on(events.InlineQuery(pattern=r"repo"))
 async def inline_id_handler(event: events.InlineQuery.Event):
- DEVIL = event.builder
+ LEGENDX = event.builder
  X= [[custom.Button.inline("🔥 CLICK ME 🔥",data="obhai")]]
  query = event.text
- result = DEVIL.article("DEVIL",text="REPO AND SUPPORT",buttons=X,link_preview=False)
+ result = LEGENDX.article("ULTRA X",text="REPO AND SUPPORT",buttons=X,link_preview=False)
  await event.answer([result])
 @xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"obhai")))
 async def callback_query_handler(event):
-
-# inline by LEGENDX22 and PROBOYX 🔥 and Betichod
+# inline by TEAMLEGEND, TEAMULTRAx
   await event.edit(text=f"ULTRA-X Repo and Group link",buttons=[[Button.url(f"ULTRA-X REPO", url="https://github.com/ULTRA-OP/ULTRA-X"), Button.url(f"ULTRA-X SUPPORT", url="https://t.me/ULTRAXCHAT")]])
