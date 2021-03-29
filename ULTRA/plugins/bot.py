859acf0b1a912e69ad822de75331dd51fe0fa9fd
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-03-29 16:21:57 +0530

    Test

--- ULTRA/plugins/bot.py
+++ ULTRA/plugins/bot.py
@@ -41,8 +41,7 @@ if TG_BOT_USER_NAME_BF_HER is not None:
                     Button.url("Rᴇᴘᴏ", "https://github.com/ULTRA-OP/ULTRA-X"),
                     Button.url("Dᴇᴘʟᴏʏ", "https://heroku.com/deploy?template=https://github.com/ULTRA-OP/ULTRA-X/blob/master")],
                     [Button.url("Sᴛʀɪɴɢ", "https://repl.it/ULTRA-OP/ULTRA-X#main.py"),
-                    Button.url("Cʜᴀɴɴᴇʟ", "https://t.me/ULTRAXOT")
-                ]
+                    Button.url("Cʜᴀɴɴᴇʟ", "https://t.me/ULTRAXOT")],
                     [Button.url("Master", "https://t.me/{ALIVE_USERNAME}),
                     Button.url("Assistant", "https://t.me/{ALIVE_BOT_USERNAME})
                 ]
