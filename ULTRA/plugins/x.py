4b49b344ebaf25a1b60e21af315b0c8e0a0f85b0
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-03-29 15:50:31 +0530

    Update x.py

--- ULTRA/plugins/x.py
+++ ULTRA/plugins/x.py
@@ -130,7 +130,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
         reply_pop_up_alert = help_string
         reply_pop_up_alert += (
-            "\n\n __Click on buttons below to load or unload them..report us if you find any bug__\n\n **©LEGEND USERBOT**".format(plugin_name)
+            "\n\n __Cʟɪᴄᴋ Oɴ Tʜᴇ Bᴜᴛᴛᴏɴs Bᴇʟᴏᴡ Tᴏ Lᴏᴀᴅ Oʀ Uɴʟᴏᴀᴅ Pʟᴜɢɪɴs..Rᴇᴘᴏʀᴛ Us Fᴏʀ Aɴʏ Hᴇʟᴘ__\n\n **© @UltraXOT**".format(plugin_name)
         )
         try:
             if event.query.user_id == bot.uid or event.query.user_id == id:
