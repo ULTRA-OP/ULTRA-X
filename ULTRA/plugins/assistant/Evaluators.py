9b40d2eb03f49c40c754fd69350a0db038bdcaea
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-30 13:29:51 +0530

    Update Evaluators.py

--- ULTRA/plugins/assistant/Evaluators.py
+++ ULTRA/plugins/assistant/Evaluators.py
@@ -1,4 +1,9 @@
 #hmm
+import asyncio
+import io
+import os
+import sys
+import traceback
 from LEGENDX import xbot, devs
 from telethon import events
 from ULTRA import bot
@@ -39,8 +44,7 @@ async def _(event):
     else:
         evaluation = "Success"
     final_output = f"**•  Eval : **\n`{cmd}` \n\n**•  Result : **\n`{evaluation}` \n"
-    await edit_or_reply(
-        proevent,
+    await proevent.edit(
         text=final_output,
         aslink=True,
         linktext=f"**•  Eval : **\n`{cmd}` \n\n**•  Result : **\n",
@@ -59,3 +63,33 @@ async def aexec(code, smessatatus):
         message, event, reply, message.client, p, message.chat_id
     )
 
+
+@xbot.on(events.NewMessage(pattern="/exec ?(*.)"))
+async def _(event):
+    pro = await bot.get_me()
+    boy = pro.id
+    if not event.sender_id in devs:
+        if not event.sender_id == boy:
+          return
+    if event.fwd_from:
+        return
+    cmd = "".join(event.message.message.split(maxsplit=1)[1:])
+    if not cmd:
+        return await event.reply("What should i execute?..")
+    proevent = await event.reply("Executing.....")
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
+    curruser = pro.username or "UltraX"
+    uid = os.geteuid()
+    if uid == 0:
+        cresult = f"`{curruser}:~#` `{cmd}`\n`{result}`"
+    else:
+        cresult = f"`{curruser}:~$` `{cmd}`\n`{result}`"
+    await proevent.edit(
+        text=cresult,
+        aslink=True,
+        linktext=f"**•  Exec : **\n`{cmd}` \n\n**•  Result : **\n",
+    )
