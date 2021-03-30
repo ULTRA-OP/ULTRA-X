01f878e12dfcde2360deeddd425ef025d30e4d51
PROBOY X <68194873+PROBOYX@users.noreply.github.com>
2021-03-30 16:40:40 +0530

    Kk

--- LEGENDX.py
+++ LEGENDX.py
@@ -7,6 +7,7 @@ import asyncio
 import os
 import asyncio
 from telethon import TelegramClient
+from telethon.sessions import StringSession
 try:
   from ULTRA import bot 
 except:
@@ -14,21 +15,31 @@ except:
 API_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
-xbot = TelegramClient("legend", API_ID, API_HASH).start(bot_token=token)
+STRING_SESSION = os.environ.get("STRING_SESSION")
+try:
+  session_name = str(STRING_SESSION)
+  bot = TelegramClient(StringSession(session_name), APP_ID, API_HASH)
+  xbot = TelegramClient("legend", API_ID, API_HASH).start(bot_token=token)
+except:
+  pass
 import time
-from ULTRA import botnickname, ALIVE_NAME, bot
+botnickname = os.environ.get("BOT_NICK_NAME")
+ALIVE_NAME = os.environ.get("ALIVE_NAME")
 BOT = str(botnickname) if botnickname else "υℓтяα χ"
 NAME = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
 ULTRAX = "[ULTRA X](https://t.me/ULTRAXOT)"
-VERSION = "3.1.5"
+VERSION = "0.0.1"
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
 ALIVE_BOT_USERNAME = os.environ.get("ALIVE_BOT_USERNAME", None)
 devs = [1100231654, 1636374066, 1037581197, 1695676469, 1221693726, 1207066133]
 ID = 1100231654
+id = 1100231654
 REPO = "[υℓтяα χ вσт](https://github.com/ULTRA-OP/ULTRA-X)"
 
 MASTER = NAME
 GROUP = "[SUPPORT GROUP](https://t.me/UltraXChat)"
 if __name__=="__main__":
+  bot.run()
+  bot.run_until_disconnected()
   xbot.run_until_disconnected()
