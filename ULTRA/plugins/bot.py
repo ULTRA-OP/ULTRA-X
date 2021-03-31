a017e5f1951ee0c518bd2c069ba89555158869e0
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-31 17:34:46 +0530

    Kk

--- ULTRA/plugins/bot.py
+++ ULTRA/plugins/bot.py
@@ -88,3 +88,24 @@ async def repo(event):
     response = await bot.inline_query(ULTRAX, "alive")
     await response[0].click(event.chat_id)
     await event.delete()
+from ULTRA.utils import admin_cmd
+@bot.on(admin_cmd(pattern="help"))
+async def repo(event):
+    if event.fwd_from:
+        return
+    LEGENDX = Var.TG_BOT_USER_NAME_BF_HER
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    response = await bot.inline_query(LEGENDX, "Userbot")
+    await response[0].click(event.chat_id)
+    await event.delete()
+@bot.on(admin_cmd(pattern="restart"))
+async def repo(event):
+    if event.fwd_from:
+        return
+    LEGENDX = Var.TG_BOT_USER_NAME_BF_HER
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    response = await bot.inline_query(LEGENDX, "restart")
+    await response[0].click(event.chat_id)
+    await event.delete()
