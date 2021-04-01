19a1b711e66f38029822719ad835bd9266aa0ec1
PROBOY X <68194873+PROBOYX@users.noreply.github.com>
2021-04-01 10:24:06 +0530

    Kk

--- ULTRA/__main__.py
+++ ULTRA/__main__.py
@@ -3,7 +3,7 @@ try:
   from LEGENDX import xbot
 except:
   os.system("pip install LEGENDX")
-  from LEGENDX import xbot
+  from ULTRAX import xbot
 from ULTRA import bot
 from sys import argv
 import sys
