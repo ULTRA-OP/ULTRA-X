8d78bdd0db4d2034200e41809ead4849a4fce66a
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-04-07 21:24:25 +0530

    Domne

---
+++ ULTRA/plugins/animation2.py
@@ -0,0 +1,226 @@
+# ported from madboy482 
+# madboy482
+
+import asyncio
+from collections import deque
+
+
+from ULTRA.utils import admin_cmd, edit_or_reply, sudo_cmd
+from ULTRA import CMD_HELP, ALIVE_NAME
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
+
+USERID = bot.uid
+
+mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
+
+
+@bot.on(admin_cmd(pattern="think$", outgoing=True))
+@bot.on(sudo_cmd(pattern="think$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "ThInKiNg..")
+    deq = deque(list("🤔🧐🤔🧐🤔🧐"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(pattern=r"lmao$"))
+@bot.on(sudo_cmd(pattern="lmao$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "LoL, Lmao")
+    deq = deque(list("😂🤣😂🤣😂🤣"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(pattern=r"nothappy$"))
+@bot.on(sudo_cmd(pattern="nothappy$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Me Nt HaPpY")
+    deq = deque(list("😁☹️😁☹️😁☹️😁"))
+    for _ in range(48):
+        await asyncio.sleep(0.4)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(outgoing=True, pattern="clock$"))
+@bot.on(sudo_cmd(pattern="clock$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Tik..Tik..Tik...")
+    deq = deque(list("🕙🕘🕗🕖🕕🕔🕓🕒🕑🕐🕛"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(pattern=r"muah$"))
+@bot.on(sudo_cmd(pattern="muah$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Muah...Muahh...My Babby...")
+    deq = deque(list("😗😙😚😚😘"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(pattern="heart$"))
+@bot.on(sudo_cmd(pattern="heart$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Dil Dhak Dhak krne lga..")
+    deq = deque(list("❤️🧡💛💚💙💜🖤"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(pattern="gym$", outgoing=True))
+@bot.on(sudo_cmd(pattern="gym$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Lemme Gym..")
+    deq = deque(list("🏃‍🏋‍🤸‍🏃‍🏋‍🤸‍🏃‍🏋‍🤸‍"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(pattern=f"earth$", outgoing=True))
+@bot.on(sudo_cmd(pattern="earth$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Ye dunia Gol h...")
+    deq = deque(list("🌏🌍🌎🌎🌍🌏🌍🌎"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(outgoing=True, pattern="moon$"))
+@bot.on(sudo_cmd(pattern="moon$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Chand ki Chandni..")
+    deq = deque(list("🌗🌘🌑🌒🌓🌔🌕🌖"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(pattern=f"smoon$", outgoing=True))
+@bot.on(sudo_cmd(pattern="smoon$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Dusre Chand ki Dusri Chandni..")
+    animation_interval = 0.1
+    animation_ttl = range(101)
+    await event.edit("smoon..")
+    animation_chars = [
+        "🌗🌗🌗🌗🌗\n🌓🌓🌓🌓🌓\n🌗🌗🌗🌗🌗\n🌓🌓🌓🌓🌓\n🌗🌗🌗🌗🌗",
+        "🌘🌘🌘🌘🌘\n🌔🌔🌔🌔🌔\n🌘🌘🌘🌘🌘\n🌔🌔🌔🌔🌔\n🌘🌘🌘🌘🌘",
+        "🌑🌑🌑🌑🌑\n🌕🌕🌕🌕🌕\n🌑🌑🌑🌑🌑\n🌕🌕🌕🌕🌕\n🌑🌑🌑🌑🌑",
+        "🌒🌒🌒🌒🌒\n🌖🌖🌖🌖🌖\n🌒🌒🌒🌒🌒\n🌖🌖🌖🌖🌖\n🌒🌒🌒🌒🌒",
+        "🌓🌓🌓🌓🌓\n🌗🌗🌗🌗🌗\n🌓🌓🌓🌓🌓\n🌗🌗🌗🌗🌗\n🌓🌓🌓🌓🌓",
+        "🌔🌔🌔🌔🌔\n🌘🌘🌘🌘🌘\n🌔🌔🌔🌔🌔\n🌘🌘🌘🌘🌘\n🌔🌔🌔🌔🌔",
+        "🌕🌕🌕🌕🌕\n🌑🌑🌑🌑🌑\n🌕🌕🌕🌕🌕\n🌑🌑🌑🌑🌑\n🌕🌕🌕🌕🌕",
+        "🌖🌖🌖🌖🌖\n🌒🌒🌒🌒🌒\n🌖🌖🌖🌖🌖\n🌒🌒🌒🌒🌒\n🌖🌖🌖🌖🌖",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 8])
+
+
+@bot.on(admin_cmd(pattern=f"tmoon$", outgoing=True))
+@bot.on(sudo_cmd(pattern="tmoon$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Teesre Chand ki Teesri Chandni..")
+    animation_interval = 0.1
+    animation_ttl = range(96)
+    await event.edit("tmoon..")
+    animation_chars = [
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 32])
+
+
+CMD_HELP.update(
+    {
+        "animation2": """**Plugin : **`animation2`
+        
+**Commands in animation2 are **
+  •  `.think`
+  •  `.lmao`
+  •  `.nothappy`
+  •  `.clock`
+  •  `.muah`
+  •  `.heart`
+  •  `.gym`
+  •  `.earth`
+  •  `.moon`
+  •  `.smoon`
+  •  `.tmoon`
+  
+**Function : **__Different kinds of animation commands check yourself for their animation .__"""
+    }
+)
