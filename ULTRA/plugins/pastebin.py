1c3c6c335c128e37573cb3685edbcda413871ee1
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-04-07 21:19:02 +0530

    .

--- ULTRA/plugins/pastebin.py
+++ ULTRA/plugins/pastebin.py
@@ -1,6 +1,5 @@
 """IX.IO pastebin like site
 Syntax: .paste
-Syntax: .neko
 Syntax: .paster
 Syntax: .iffuci
 """
@@ -133,63 +132,6 @@ async def get_dogbin_content(dog_url):
             "Get dogbin content query was executed successfully",
         )
 
-
-@borg.on(admin_cmd(pattern="neko ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    datetime.now()
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    input_str = event.pattern_match.group(1)
-    message = "SYNTAX: `.neko <long text to include>`"
-    if input_str:
-        message = input_str
-    elif event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        if previous_message.media:
-            downloaded_file_name = await borg.download_media(
-                previous_message,
-                Config.TMP_DOWNLOAD_DIRECTORY,
-                progress_callback=progress,
-            )
-            m_list = None
-            with open(downloaded_file_name, "rb") as fd:
-                m_list = fd.readlines()
-            message = ""
-            for m in m_list:
-                # message += m.decode("UTF-8") + "\r\n"
-                message += m.decode("UTF-8")
-            os.remove(downloaded_file_name)
-        else:
-            message = previous_message.message
-    else:
-        message = "SYNTAX: `.neko <long text to include>`"
-    if downloaded_file_name.endswith(".py"):
-        py_file = ""
-        py_file += ".py"
-        data = message
-        key = (
-            requests.post("https://nekobin.com/api/documents", json={"content": data})
-            .json()
-            .get("result")
-            .get("key")
-        )
-        url = f"https://nekobin.com/{key}{py_file}"
-    else:
-        data = message
-        key = (
-            requests.post("https://nekobin.com/api/documents", json={"content": data})
-            .json()
-            .get("result")
-            .get("key")
-        )
-        url = f"https://nekobin.com/{key}"
-
-    reply_text = f"Pasted to Nekobin : [neko]({url})"
-    await event.edit(reply_text)
-
-
 @borg.on(admin_cmd(pattern="iffuci ?(.*)"))
 async def _(event):
     if event.fwd_from:
@@ -301,8 +243,6 @@ CMD_HELP.update(
 \nUsage: Create a paste or a shortened url using dogbin (https://del.dog/)\
 \n\n.getpaste\
 \nUsage: Gets the content of a paste or shortened url from dogbin (https://del.dog/)\
-\n\n.neko <reply>\
-\nUsage: Create a paste or a shortened url using nekobin (https://nekobin.com)\
 \n\n.iffuci <text/reply>\
 \nUsage: Create a paste or a shortened url using iffuci (https://www.iffuci.tk)\
 \n\n.paster <text/reply>\
