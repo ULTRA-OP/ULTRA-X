64e938f36372960cee6ef71320af54aea2a35c2f
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-08 14:00:10 +0530

    Update federation.py

--- ULTRA/plugins/assistant/federation.py
+++ ULTRA/plugins/assistant/federation.py
@@ -1,1 +1,9 @@
 #dmca
+#accha bsdk denzid ko ni dekskta pr tu leskta ye kya vakchodi he
+#mera file he agar firse dala to pakka will conatact GitHub
+#fuckkoff
+#Weiss bhi poora me hi likha dha
+#smd
+#remove krde sara repo se collab me jarha
+#fuck teamultrax
+#leabing tg be bk on 23rd
