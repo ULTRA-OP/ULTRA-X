f24b8b03e0eb89d1e2af22c4c3203a8df8f7930f
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-04-08 21:11:09 +0530

    oo

--- ULTRA/plugins/logs.py
+++ ULTRA/plugins/logs.py
@@ -1,25 +1,10 @@
 #BY SH1VAM DONT KANG
 #BY SH1VAM DONT KANG
-#BY SH1VAM DONT KANG
-#BY SH1VAM DONT KANG
-#BY SH1VAM DONT KANG
-#BY SH1VAM DONT KANG
-
-
-
-
-
-
 import os
 
 
 from telethon import Button, custom, events, functions
   
-
-
-
-
-
 import math
 import heroku3
 
@@ -37,16 +22,16 @@ async def inline_id_handler(event: events.InlineQuery.Event):
         try:
             Heroku = heroku3.from_key(HEROKU_API_KEY);app = Heroku.app(HEROKU_APP_NAME)
         except:
-            shivamslog = builder.article(title="Cant Get It",description="Configuration Error",text="Please Check Heroku App Name It Must Be Same As Your App Name Or Check Heroku Api Key U Can Regenerate From https://dashboard.heroku.com/account Scroll Down Regenerate And Change In settings Of App U Need To Reveal Config Vars",buttons=[[Button.switch_inline("Search Again", query="logs", same_peer=True)],], )
+            shivamslog = builder.article(title="Cant Get It",description="Configuration Error",text="Please Check `HEROKU_APP_NAME`, It Must Be Same As Your App Name Or Check `HEROKU_API_KEY`; U Can Regenerate From https://dashboard.heroku.com/account, Scroll Down Regenerate And Change In settings Of App U Need To Reveal Config Vars",buttons=[[Button.switch_inline("Search Again", query="logs", same_peer=True)],], )
             await event.answer([shivamslog])
             return
         else:
             with open('logs.txt', 'w') as log:
                 log.write(app.get_log())
-            shivamlog=builder.document("logs.txt",title="Logs",description="logs of 100+ lines use .paste to past in dogbin",text="use .paste to paste inn dogbin",buttons=[[Button.switch_inline("Search Again", query="logs", same_peer=True)],], )
+            shivamlog=builder.document("logs.txt",title="Logs",description="Here iz your HEROKU APP Logs",text="Use .paste to paste in dogbin or .neko to paste in nekobin",buttons=[[Button.switch_inline("Search Again", query="logs", same_peer=True)],], )
             await event.answer([shivamlog])
             return os.remove('logs.txt')
     if not event.query.user_id == me.id:
-        resultm = builder.article(title="me not your bot",description="Mind Your Business",text="Hey U Must Use https://github.com/legendx22/LEGEND-BOT ",buttons=[[Button.switch_inline("Search Again", query="logs", same_peer=True)],], )
+        resultm = builder.article(title="Me iz not your Bot",description="Mind Your Own Bizness",text="Hey!! U Must Use https://github.com/ULTRA-OP/ULTRA-X",buttons=[[Button.switch_inline("Search Again", query="logs", same_peer=True)],], )
         await event.answer([resultm])
         return
