3ca3a1dc0c301014398fb3417d56bbae089fd023
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-04-09 13:26:32 +0530

    Updated

--- ULTRA/plugins/repo.py
+++ ULTRA/plugins/repo.py
@@ -1,18 +1,54 @@
-
+# updated by madboy482
+# updated by madboy482
+# updated by madboy482
 from telethon import events, Button, custom
 from ULTRA import bot
 from ULTRAX import xbot
+# updated by madboy482
+# updated by madboy482
+# updated by madboy482
 import os,re
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
 @xbot.on(events.InlineQuery(pattern=r"repo"))
 async def inline_id_handler(event: events.InlineQuery.Event):
+ # updated by madboy482
+ # updated by madboy482
+ # updated by madboy482
  LEGENDX = event.builder
- X= [[custom.Button.inline("🔥 CLICK ME 🔥",data="obhai")]]
+ X= [[custom.Button.inline("🔥 Cʟɪᴄᴋ Hᴇʀᴇ 🔥",data="obhai")]]
  query = event.text
- result = LEGENDX.article("ULTRA X",text="REPO AND SUPPORT",buttons=X,link_preview=False)
+ # updated by madboy482
+ # updated by madboy482
+ # updated by madboy482
+ result = LEGENDX.article("UʟᴛʀᴀX",text="**UltraX's Rᴇᴘᴏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @UltraXOT**",buttons=X,link_preview=False)
+ # updated by madboy482
+ # updated by madboy482
+ # updated by madboy482
  await event.answer([result])
 @xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"obhai")))
+# updated by madboy482
+# updated by madboy482
+# updated by madboy482
 async def callback_query_handler(event):
-# inline by TEAMLEGEND, TEAMULTRAx
-  await event.edit(text=f"ULTRA-X Repo and Group link",buttons=[[Button.url(f"ULTRA-X REPO", url="https://github.com/ULTRA-OP/ULTRA-X"), Button.url(f"ULTRA-X SUPPORT", url="https://t.me/ULTRAXCHAT")]])
+# inline by TEAMLEGEND, TEAMULTRAX
+  await event.edit(text=f"**UʟᴛʀᴀX's Rᴇᴘᴏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @UltraXOT**",buttons=[
+   # updated by madboy482
+   # updated by madboy482
+   # updated by madboy482
+                [
+                    Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/ULTRA-OP/ULTRA-X"),
+                 # updated by madboy482
+                 # updated by madboy482
+                 # updated by madboy482
+                    Button.url(f"🌚 Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ 🌝", url="https://t.me/ULTRAXCHAT")],
+   # updated by madboy482
+   # updated by madboy482
+   # updated by madboy482
+                    [Button.url(f"🔰 Dᴇᴘʟᴏʏ 🔰", url="https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FULTRA-OP%2FHEROKU&template=https%3A%2F%2Fgithub.com%2FULTRA-OP%2FHEROKU"),
+                ]
+            ]
+                   # updated by madboy482
+                   # updated by madboy482
+                   # updated by madboy482
+                  )
