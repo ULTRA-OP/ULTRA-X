ee6d374b7a854876bc274b40c82dc9106a12f19a
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-04-10 14:00:48 +0530

    Hmm

--- ULTRA/plugins/whishper.py
+++ ULTRA/plugins/whishper.py
@@ -1,18 +1,45 @@
-#Made by@LEGENDX22 for @LEGEND_USERBOT_SUPPORT
+#COPYRIGHT 2021-22 BY LEGENDX22, PROBOYX
+""
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+             DONT DARE TO KANG WITHOUT CREDITS
+"""
+# IF YOU KANG THEN KEEP CREDITS PLEASE 🥺
+from telethon import events, Button
+import re, os
+from ULTRAX import id
+from ULTRAX import xbot
+@xbot.on(events.InlineQuery(pattern='wspr'))
+async def inline_proboy(event):
+  PROBOYX = event.text[5:]
+  PROBOYX, GODBOYX = PROBOYX.split('@')
+  os.system("rm -rf wspr.txt")
+  f = open("wspr.txt", "a")
+  f.write(PROBOYX + "\n" + GODBOYX)
+  f.close()
+  LEGENDX = event.builder
+  MADBOI = [[Button.inline("SEE MSG", data='secret')]]
+  MADBOI += [[Button.switch_inline("REPLY", query="wspr", same_peer=True)]]
+  ALAIN = LEGENDX.article(title=f"FOR @{GODBOYX}", text=f"THIS IS SECRET MESSAGE FOR @{GODBOYX}", buttons=MADBOI)
+  await event.answer([ALAIN])
 
-from ULTRA.utils import admin_cmd, sudo_cmd, edit_or_reply
-from var import Var
-
-
-
-@borg.on(admin_cmd(pattern="wspr ?(.*)"))
+@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b'secret')))
 async def wspr(event):
-    if event.fwd_from:
-        return
-    wwwspr = event.pattern_match.group(1)
-    botusername = "@whisperBot"
-    if event.reply_to_msg_id:
-        reply_to_id = await event.get_reply_message()
-    tap = await bot.inline_query(botusername, wwwspr) 
-    await tap[0].click(event.chat_id)
-    await event.delete()
+  try:
+    f = open("wspr.txt")
+    LEGENDX = f.readlines()[0]
+    f.close()
+    pro = open("wspr.txt")
+    PROBOYX = pro.readlines()[1].lower()
+    pro.close()
+    bot = await xbot.get_me()
+    sender = f"{event.sender.username}".lower()
+    me = f"{borg.me.username}".lower()
+    if sender == PROBOYX or sender == me or event.sender_id == id:
+       await event.answer(LEGENDX, alert=True)
+    else:
+       await event.answer("HEY DON'T SEE MY MSG", alert=False)
+  except:
+    await event.answer(f"use @{bot.username} wspr msg <@ sender username>", alert=True)
