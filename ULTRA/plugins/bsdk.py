1d77bde26573398ed64e660cd026300108bac780
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-04-13 21:03:15 +0530

    try

--- ULTRA/plugins/bsdk.py
+++ ULTRA/plugins/bsdk.py
@@ -53,5 +53,31 @@ async def hehe (event):
   else:
     pass
 
+
+from telethon import events,Button
+from telethon.tl.types import InputWebDocument
+from ULTRAX import MASTER, xbot
+from ULTRA import ALIVE_NAME
+
+from ULTRA import bot as ultra
+global ok
+madboi = ultra.uid
+
+acha = ".Resources/IMG_20210413_155725_416.jpg"
+omk = acha
+omk = "https://telegra.ph/file/79f0a1b254b4511181afa.jpg"
+k = bot.me.first_name
+
+@xbot.on(events.InlineQuery())
+async def inline (event):
+  if event.query.user_id != bot.me.id or event.query.user_id ==bot.me.id and event.text == '' :
+     Buttonss = [[Button.url("Rᴇᴘᴏsɪᴛᴏʀʏ","https://github.com/ULTRA-OP/ULTRA-X"),Button.url("Dᴇᴘʟᴏʏ","https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FULTRA-OP%2FHEROKU&template=https%3A%2F%2Fgithub.com%2FULTRA-OP%2FHEROKU")]]
+     fuck = f"**UʟᴛʀᴀX - Usᴇʀʙᴏᴛ**\n**➖➖➖➖➖➖➖➖➖➖**\n**Oᴡɴᴇʀ: [{MASTER}](tg://user?id={madboi})**\n**Cʜᴀɴɴᴇʟ: @UltraX_Support**\n**Sᴜᴘᴘᴏʀᴛ: @UltraXChat**\n**➖➖➖➖➖➖➖➖➖➖**"
+     op = event.builder
+     omg = op.article(title='UʟᴛʀᴀX Usᴇʀʙᴏᴛ', text=fuck, thumb=InputWebDocument(omk, 0, "image/jpeg", []), url="t.me/UltraXOT", description="© TᴇᴀᴍUʟᴛʀᴀX | Usᴇʀʙᴏᴛ | UʟᴛʀᴀX", buttons=Buttonss)
+     await event.answer([omg], switch_pm=f'👤 Assɪsᴛᴀɴᴛ ᴏғ : {k}', switch_pm_param='start')
+  else:
+    pass
+
 CMD_HELP.update({
     "bsdk":"**Abuse Plug Do** `.bsdk`"})
