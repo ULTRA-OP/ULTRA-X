76016ebf375e09d39fbac65bf76e996c4f903df8
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-05-06 21:27:09 +0530

    Sudo

--- ULTRA/plugins/button.py
+++ ULTRA/plugins/button.py
@@ -1,28 +1,40 @@
-# COPYRIGHT (C) 2021-2022 © Ultra X Bot
-from ULTRA.utils import admin_cmd, sudo_cmd, edit_or_reply as eor
-from ULTRAX import xbot, NAME
-from telethon import Button
+# COPYRIGHT (C) 2021-2022 BY LEGENDX22
+# MADE BY LEGENDX22
+from telethon import Button, events
+from ..utils import admin_cmd
+from ..utils import edit_or_reply, eor
+from ..utils import sudo_cmd
+from .. import CMD_HELP
+
 @borg.on(admin_cmd(pattern="button (.*)"))
 @borg.on(sudo_cmd(pattern="button", allow_sudo=True))
 async def Buttons(event):
-  await eor(event, "`Mᴀᴋɪɴɢ Yᴏᴜʀ Bᴜᴛᴛᴏɴ ᴡᴇɪᴛ ᴍᴀsᴛᴇʀ !!!`")
-  ULTRAX = Var.TG_BOT_USER_NAME_BF_HER
-  pro = event.text[7:]
-  pro, boy = pro.split("|")
-  if "LEGENDX" == "PROBOYX":
-    await xbot.send_message(event.chat_id, "buttons")
-  else:
-    try:
-      async with bot.conversation(ULTRAX) as proboyx:
-        await proboyx.send_message("/start")
-        await proboyx.get_response()
-        await proboyx.send_message("my button 🥺")
-        await xbot.send_message(bot.me.id, f"{pro}", buttons=[[Button.url(f"{pro}", f"{boy}")]])
-        pro = await proboyx.get_response()
-        await pro.forward_to(event.chat_id)
-        await event.delete()
-    except:
-        await event.edit("example:\n.button <button name>|<link>\n`.button ULTRAX|https://t.me/ULTRAXOT`\nmake sure your name and link no have Useless spece ",link_preview=False)
+    await eor(event, "`Mᴀᴋɪɴɢ Yᴏᴜʀ Bᴜᴛᴛᴏɴ ᴡᴇɪᴛ ᴍᴀsᴛᴇʀ !!!`")
+    ULTRAX = Var.TG_BOT_USER_NAME_BF_HER
+    pro = event.text[7:]
+    pro, boy = pro.split("|")
+    f = open("Button.txt", "w") # by LEGENDX22, PROBOYX
+    f.write(f'{pro}\n{boy}')
+    f.close()
+    LEGENDX = await bot.inline_query(ULTRAX, "BUTTON")
+    await LEGENDX[0].click(event.chat_id)
+    await event.delete()
 
-from .. import CMD_HELP
-CMD_HELP.update({"button": "example:\n.button <button name>|<link>\n`.button ULTRAX|https://t.me/ULTRAXOT`\nmake sure your name and link no have Useless spece"})
+@xbot.on(events.InlineQuery(pattern='BUTTON'))
+async def file(event):
+  f = open("Button.txt")
+  ok = f.readlines()[0]
+  f.close()
+  PROBOYX = open("Button.txt")
+  bc = PROBOYX.readlines()[1]
+  PROBOYX.close()
+  LEGENDX = event.builder
+  LEGENDX22 = [[Button.url(f'{ok}', f'{bc}')]]
+  PROBOYXOP = LEGENDX.article(title='Button by ULTRA X', text=f'{ok}', buttons=LEGENDX22)
+  await event.answer([PROBOYXOP])
+
+CMD_HELP.update(
+    {
+        "buttons": ".button <button name>|<link>\n`.button DaisyX|https://t.me/DAISYXOT`\nmake sure your name and link no have Useless spece"
+    }
+)
