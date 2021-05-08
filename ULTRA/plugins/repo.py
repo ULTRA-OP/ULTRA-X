2bbd05285a3d5b4661026bfc3b2b9b3054131f25
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-05-08 20:03:45 +0530

    Update repo.py

--- ULTRA/plugins/repo.py
+++ ULTRA/plugins/repo.py
@@ -1,29 +1,18 @@
-
+# COPYRIGHT (C) 2021-22 BY LEGENDX22
 from telethon import events, Button, custom
-from DaisyX import bot
-from Assist.DAISYX import xbot
-
+from telethon import events, Button, custom
+from ULTRA import bot
+from ULTRAX import xbot, BOT
 import os,re
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
 @xbot.on(events.InlineQuery(pattern=r"repo"))
 async def inline_id_handler(event: events.InlineQuery.Event):
- 
- DAISY = event.builder
+ LEGENDX = event.builder
  X= [[custom.Button.inline("🔥 Cʟɪᴄᴋ Hᴇʀᴇ 🔥",data="obhai")]]
  query = event.text
- 
- result = DAISY.article("ᴅᴀɪsʏX",text="**ᴅᴀɪsʏX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @DaisyXOT**",buttons=X,link_preview=False)
+ result = LEGENDX.article("ᴅᴀɪsʏX",text="**ᴅᴀɪsʏX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @ULTRAXOT**",buttons=X,link_preview=False)
  await event.answer([result])
 @xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"obhai")))
 async def callback_query_handler(event):
-  await event.edit(text=f"**ᴅᴀɪsʏX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @DaisyXOT**",buttons=[
-                [
-                    Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/TeamDaisyX/Daisy-X-UB"),
-                    Button.url(f"🌚 Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ 🌝", url="https://t.me/DaisySupport_Official")],
-                    [Button.url(f"🔰 Dᴇᴘʟᴏʏ 🔰", url="https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FTeamDaisyX%2FDaisy-X-UB&template=https%3A%2F%2Fgithub.com%2FTeamDaisyX%2FDaisy-X-UB"),
-                     Button.url(f"💠 Sᴛʀɪɴɢ 💠", url="https://replit.com/@legendx22/ULTRA-X#main.py"),
-                ]
-            ]
-                   
-                  )
+  await event.edit(text=f"**{BOT} Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @ULTRAXOT**",buttons=[Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/ULTRA-OP/ULTRA-X")])
