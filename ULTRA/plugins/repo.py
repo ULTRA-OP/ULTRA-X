499b281cf8158ae5b1e49d74bf9785c912a898da
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-05-08 20:34:31 +0530

    Update repo.py

--- ULTRA/plugins/repo.py
+++ ULTRA/plugins/repo.py
@@ -1,18 +1,55 @@
-# COPYRIGHT (C) 2021-22 BY LEGENDX22
-from telethon import events, Button, custom
+# updated by madboy482
+# updated by madboy482
+# updated by madboy482
 from telethon import events, Button, custom
 from ULTRA import bot
-from ULTRAX import xbot, BOT
+from ULTRAX import xbot
+# updated by madboy482
+# updated by madboy482
+# updated by madboy482
 import os,re
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
 @xbot.on(events.InlineQuery(pattern=r"repo"))
 async def inline_id_handler(event: events.InlineQuery.Event):
+ # updated by madboy482
+ # updated by madboy482
+ # updated by madboy482
  LEGENDX = event.builder
  X= [[custom.Button.inline("🔥 Cʟɪᴄᴋ Hᴇʀᴇ 🔥",data="obhai")]]
  query = event.text
- result = LEGENDX.article("ᴅᴀɪsʏX",text="**ᴅᴀɪsʏX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @ULTRAXOT**",buttons=X,link_preview=False)
+ # updated by madboy482
+ # updated by madboy482
+ # updated by madboy482
+ result = LEGENDX.article("UʟᴛʀᴀX",text="**UltraX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @UltraXOT**",buttons=X,link_preview=False)
+ # updated by madboy482
+ # updated by madboy482
+ # updated by madboy482
  await event.answer([result])
 @xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"obhai")))
+# updated by madboy482
+# updated by madboy482
+# updated by madboy482
 async def callback_query_handler(event):
-  await event.edit(text=f"**{BOT} Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @ULTRAXOT**",buttons=[Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/ULTRA-OP/ULTRA-X")])
+# inline by TEAMLEGEND, TEAMULTRAX
+  await event.edit(text=f"**UʟᴛʀᴀX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @UltraXOT**",buttons=[
+   # updated by madboy482
+   # updated by madboy482
+   # updated by madboy482
+                [
+                    Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/ULTRA-OP/ULTRA-X"),
+                 # updated by madboy482
+                 # updated by madboy482
+                 # updated by madboy482
+                    Button.url(f"🌚 Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ 🌝", url="https://t.me/ULTRAXCHAT")],
+   # updated by madboy482
+   # updated by madboy482
+   # updated by madboy482
+                    [Button.url(f"🔰 Dᴇᴘʟᴏʏ 🔰", url="https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FULTRA-OP%2FHEROKU&template=https%3A%2F%2Fgithub.com%2FULTRA-OP%2FHEROKU"),
+                     Button.url(f"💠 Sᴛʀɪɴɢ 💠", url="https://replit.com/@legendx22/ULTRA-X#main.py"),
+                ]
+            ]
+                   # updated by madboy482
+                   # updated by madboy482
+                   # updated by madboy482
+                  )
