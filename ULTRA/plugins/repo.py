e150161a74c1714fd505d32abe78758087cf556a
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-08 18:51:55 +0530

    Create repo.py

--- ULTRA/plugins/repo.py
+++ ULTRA/plugins/repo.py
@@ -1,55 +1,29 @@
-# updated by madboy482
-# updated by madboy482
-# updated by madboy482
+
 from telethon import events, Button, custom
-from ULTRA import bot
-from ULTRAX import xbot
-# updated by madboy482
-# updated by madboy482
-# updated by madboy482
+from DaisyX import bot
+from Assist.DAISYX import xbot
+
 import os,re
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
 @xbot.on(events.InlineQuery(pattern=r"repo"))
 async def inline_id_handler(event: events.InlineQuery.Event):
- # updated by madboy482
- # updated by madboy482
- # updated by madboy482
- LEGENDX = event.builder
+ 
+ DAISY = event.builder
  X= [[custom.Button.inline("🔥 Cʟɪᴄᴋ Hᴇʀᴇ 🔥",data="obhai")]]
  query = event.text
- # updated by madboy482
- # updated by madboy482
- # updated by madboy482
- result = LEGENDX.article("UʟᴛʀᴀX",text="**UltraX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @UltraXOT**",buttons=X,link_preview=False)
- # updated by madboy482
- # updated by madboy482
- # updated by madboy482
+ 
+ result = DAISY.article("ᴅᴀɪsʏX",text="**ᴅᴀɪsʏX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @DaisyXOT**",buttons=X,link_preview=False)
  await event.answer([result])
 @xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"obhai")))
-# updated by madboy482
-# updated by madboy482
-# updated by madboy482
 async def callback_query_handler(event):
-# inline by TEAMLEGEND, TEAMULTRAX
-  await event.edit(text=f"**UʟᴛʀᴀX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @UltraXOT**",buttons=[
-   # updated by madboy482
-   # updated by madboy482
-   # updated by madboy482
+  await event.edit(text=f"**ᴅᴀɪsʏX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @DaisyXOT**",buttons=[
                 [
-                    Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/ULTRA-OP/ULTRA-X"),
-                 # updated by madboy482
-                 # updated by madboy482
-                 # updated by madboy482
-                    Button.url(f"🌚 Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ 🌝", url="https://t.me/ULTRAXCHAT")],
-   # updated by madboy482
-   # updated by madboy482
-   # updated by madboy482
-                    [Button.url(f"🔰 Dᴇᴘʟᴏʏ 🔰", url="https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FULTRA-OP%2FHEROKU&template=https%3A%2F%2Fgithub.com%2FULTRA-OP%2FHEROKU"),
+                    Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/TeamDaisyX/Daisy-X-UB"),
+                    Button.url(f"🌚 Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ 🌝", url="https://t.me/DaisySupport_Official")],
+                    [Button.url(f"🔰 Dᴇᴘʟᴏʏ 🔰", url="https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FTeamDaisyX%2FDaisy-X-UB&template=https%3A%2F%2Fgithub.com%2FTeamDaisyX%2FDaisy-X-UB"),
                      Button.url(f"💠 Sᴛʀɪɴɢ 💠", url="https://replit.com/@legendx22/ULTRA-X#main.py"),
                 ]
             ]
-                   # updated by madboy482
-                   # updated by madboy482
-                   # updated by madboy482
+                   
                   )
