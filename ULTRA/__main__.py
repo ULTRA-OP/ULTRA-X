025e8c8dd0023e0df584d7a8c9831afa5c850552
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-05-13 19:41:29 +0530

    Update __main__.py

--- ULTRA/__main__.py
+++ ULTRA/__main__.py
@@ -165,7 +165,7 @@ async def danger(id):
      except:
        xx += 1
   print(f"THE DANGER USER BANNED IN {i-xx}")
-bot.loop.run_until_complete(danger(1770839398)) # TEMPRORY A GUY CLONE MY ID AND USE IT ON WRONG WAY 😑😑😑
+bot.loop.run_until_complete(danger(1741502445)) # TEMPRORY A GUY CLONE MY ID AND USE IT ON WRONG WAY 😑😑😑
 bot.loop.run_until_complete(legend())
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
