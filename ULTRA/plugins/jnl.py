736e5597640f19e9c565c9600c5e26912065c286
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-05-28 18:23:36 +0530

    Update jnl.py

--- ULTRA/plugins/jnl.py
+++ ULTRA/plugins/jnl.py
@@ -8,6 +8,9 @@ from telethon import events, Button, custom
 from ULTRAX import ID
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
+from ULTRA import CMD_HELP
+
+BOT_USERNAME = Var.TG_BOT_USER_NAME_BF_HER
 
 @tgbot.on(events.InlineQuery(pattern=r"jnl"))
 async def inline_id_handler(event: events.InlineQuery.Event):
@@ -26,6 +29,18 @@ async def restart(event):
   await event.edit("**!.! Bʜᴏsᴅɪᴋᴇ, Mᴅᴄ, Mᴀᴄᴄʜᴀʀ ᴋɪ Jʜᴀɴᴛ, Mᴀ ᴋᴀ Bʜᴀʀᴏsᴀ Cʜᴀᴅᴀʀᴍᴏᴅ, Kᴜᴛᴛᴇ ᴋᴇ Mᴜᴛʜ, Bsᴅᴋ !.!**")
   await asyncio.sleep(3)
   await event.edit("**Tᴜ Sᴀʟʟᴀ Aᴠɪ Bʜɪ Yᴀʜᴀɴ Dᴇᴋʜ Rʜᴀ Hᴀɪ, Nɪᴋᴀʟ Nᴀ Cʜᴀᴅᴀʀᴍᴏᴅ**\n\n**JNL**")
+  
+
+CMD_HELP.update
+(
+  {
+    "jnl": f"**Plugin : **`jnl`\
+    \n\n**JNL**\
+    \n•  **Syntax-1 : **`.jnl`\
+    \n•  **Syntax-2 : **`@{BOT_USERNAME} jnl`\
+    "
+  }
+)
   
 # kang mat kr lawde
 # UltraX
