2d03d5937f32aacb1162b42a3f5c5fe27e2a4745
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-05-30 14:09:34 +0530

    Doone

--- ULTRA/plugins/linux_bash.py
+++ ULTRA/plugins/linux_bash.py
@@ -12,7 +12,7 @@ async def _(event):
         return
     DELAY_BETWEEN_EDITS = 0.3
     PROCESS_RUN_TIME = 100
-    cmd = event.pattern_match.group(1)
+    cmd = event.text.split(" ", 1)[1]
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
