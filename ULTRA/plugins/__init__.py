00b948feb59ebbacea1b39976c26c8db951aa99b
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-05-30 13:50:19 +0530

    Update __init__.py
    Import fixed

--- ULTRA/plugins/__init__.py
+++ ULTRA/plugins/__init__.py
@@ -1,6 +1,10 @@
 from ULTRA.helpers import functions as simpdef
 from LEGENDX import *
 from LEGENDX.ULTRAX import *
+from ULTRA import *
+from ULTRA.utils import *
+from ULTRAX import *
+
 simpmusic = simpdef.simpmusic 
 simpmusicvideo = simpdef.simpmusicvideo
 
