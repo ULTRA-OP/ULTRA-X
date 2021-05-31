f5fb192a12d920fe9f0475967a2bf58e34f1460a
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-05-31 09:44:27 +0530

    Create dm.py

---
+++ ULTRA/plugins/dm.py
@@ -0,0 +1,44 @@
+from telethon import *
+
+from ULTRA import CMD_HELP
+from ULTRA.utils import admin_cmd
+from ULTRAX import MASTER
+
+
+# Fixed by LEGDND X
+@borg.on(admin_cmd(pattern="dm ?(.*)"))
+async def _(dc):
+
+    d = dc.pattern_match.group(1)
+
+    c = d.split(" ")
+
+    chat_id = c[0]
+    try:
+        chat_id = int(chat_id)
+
+    except BaseException:
+
+        pass
+
+    msg = ""
+    masg = await dc.get_reply_message()  # ghanta😒😒
+    if dc.reply_to_msg_id:
+        await borg.send_message(chat_id, masg)
+        await dc.edit(f"✅ **{MASTER}:** Your message was successfully delivered ✅")
+    for i in c[1:]:
+        msg += i + " " 
+    if msg == "":  # hoho
+        return
+    try:
+        await borg.send_message(chat_id, msg)
+        await dc.edit(f"✅ **{MASTER}:** Your message was successfully delivered ✅")
+    except BaseException:  # hmmmmmmmmm🤔🤔
+        await dc.edit(".dm (username) (text)")
+
+
+CMD_HELP.update(
+    {
+        "dm": ".dm (username) (text)\n or\n .dm (username)(reply to msg)\n it'll forward the replyed msg"
+    }
+)
