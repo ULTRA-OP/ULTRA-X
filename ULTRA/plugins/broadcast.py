b14e62b9ac4e35e17d68762b31aec56fac8d152f
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-06-04 18:29:28 +0530

    Create broadcast.py

---
+++ ULTRA/plugins/broadcast.py
@@ -0,0 +1,19 @@
+from ..utils import admin_cmd
+@bot.on(admin_cmd(pattern='broadcast'))
+async def broadcast(event):
+  Pro = event.text[10:]
+  async for PROBOYX in bot.iter_dialogs():
+     fail = []
+     succ = []
+     chat = PROBOYX.id
+     if PROBOYX.is_channel:
+       pass
+     try:
+        await bot.send_message(chat, Pro)
+        succ.append(1)
+     except:
+        fail.append(1)
+        pass
+  omk = len(succ)
+  shit = len(fail)
+  await event.edit(f' done = {omk}\n fail = {shit}')
