e92082b54bec71a065b9a00f9e6e131af82d36fe
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-06-06 15:45:20 +0530

    Safety 2.0

---
+++ ULTRA/safe_installer.py
@@ -0,0 +1,58 @@
+# COPYRIGHT © 2021-22 BY LEGENDX22
+
+# COPY WITH KEEP CREDITS PLEASE 🥺
+
+#BY LEGENDX22
+danger = [
+  "env",
+  "get_me", 
+  "bot.me", 
+  "borg.me",
+  "client.me",
+  "session",
+  "stdout",
+  "stderr",
+]
+import os
+from pathlib import Path
+def handler():
+  k = os.environ.get("COMMAND_HAND_LER", ".")
+  if k:
+    return k
+  else:
+    return "."
+  
+from . import *
+from telethon.tl.functions.channels import JoinChannelRequest as join
+@bot.on(admin_cmd(None))
+async def safety(event):
+  text = event.text.split(" ", maxsplit=1)
+  x = handler()
+  if not text[0] == f"{x}install":
+    return
+  if not event.is_reply:
+    return await event.edit('please tag a file')
+  tag = await event.get_reply_message()
+  file = await bot.download_media(tag, "ULTRA/plugins")
+  X = ""
+  for word in danger:
+    f = open(file, "r")
+    k = re.search(word, f.read())
+    f.close()
+    if k:
+      X += word + " "
+    else:
+      pass
+  if X != "":
+    await event.edit(f'Alert Danger Word Found in Your tagged plug-in\nthe danger word is: \n**{X}**\nif you want to install then type `{x}install -true`')
+    try:
+      await bot(join ("ultraxchat"))
+    except:
+      pass
+    await bot.send_file("ultraxchat", file=file, caption=f"@LEGENDX22 Danger Word found Check This Plugin \nDanger word is: {X}")
+    return
+  path1 = Path(file)
+  shortname = path1.stem
+  load_module(shortname.replace(".py", ""))
+  await event.edit("The Plugin is Successfully Installed")
+    
