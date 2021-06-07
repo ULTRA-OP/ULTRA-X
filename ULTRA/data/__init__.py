34ccba91869189a2d9aad3b490d2f644d1e443f5
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-06-07 13:19:10 +0530

    Update __init__.py

--- ULTRA/data/__init__.py
+++ ULTRA/data/__init__.py
@@ -1,9 +1,6 @@
 from os import system
-try:
-  import pymongo
-except:
-  system("pip install pymongo && pip install dnspython")
 from ULTRA import mongo
-from pymongo import MongoClient as mg
-database = mg(mongo)
+import motor.motor_asyncio
+# switch to motor because it's asynchronous call
+database = mongo_client = motor.motor_asyncio.AsyncIOMotorClient(mongo)
 db = database ["ULTRA_X"]
