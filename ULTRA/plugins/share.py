01fedbe10bd47a2b68bc7ef204d972da5497541e
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-06-08 18:37:04 +0530

    k

--- ULTRA/plugins/share.py
+++ ULTRA/plugins/share.py
@@ -1,5 +1,6 @@
 # COPYRIGHT © 2021-2022 BY LEGENDX22
 # COPY WITH CREDITS
+
 from . import *
 from telethon import *
 def handler():
@@ -11,7 +12,7 @@ async def amazing (event):
   text = event.text.split(" ", 1)
   #hndlr = handler()
   #if event.text == f'{hndlr}share':
-    #return await event.edit("Please Give some text")
+    #return await event.edit("𝐏𝐥𝐞𝐚𝐬𝐞 𝐠𝐢𝐯𝐞 𝐬𝐨𝐦𝐞 𝐓𝐞𝐱𝐭")
   pro = text[1].replace(" ", "%20")
   inline = await bot.inline_query((await xbot.get_me()).username, f'share|{pro}')
   print ("share plug-in fired")
@@ -23,6 +24,6 @@ async def share_inline(event):
   text = event.text.split("|")
   url = f'http://t.me/share/url?url={text[1]}'
   LEGENDX = event.builder
-  pro = [Button.url("TAP ME", url=url)]
-  piro = LEGENDX.article(title='Shared Message', text="Super Message tap on Button", buttons=pro)
+  pro = [Button.url("𝑪𝒍𝒊𝒄𝒌 𝑯𝒆𝒓𝒆", url=url)]
+  piro = LEGENDX.article(title='Super Message', text="𝙎𝙪𝙥𝙚𝙧 𝙈𝙚𝙨𝙨𝙖𝙜𝙚\n\n𝙏𝙖𝙥 𝙤𝙣 𝙩𝙝𝙚 𝘽𝙪𝙩𝙩𝙤𝙣 𝘽𝙚𝙡𝙤𝙬, 𝙖𝙣𝙙 𝙎𝙚𝙡𝙚𝙘𝙩 𝙩𝙝𝙚 𝘾𝙝𝙖𝙩 𝙩𝙤 𝙨𝙚𝙣𝙙 𝙩𝙝𝙚 𝙈𝙚𝙨𝙨𝙖𝙜𝙚", buttons=pro)
   await event.answer([piro])
