553da50bd58c41d24b3253ef72ca4ebf65f7a5bf
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-06-08 18:43:31 +0530

    Create usersdata_db.py

---
+++ ULTRA/data/usersdata_db.py
@@ -0,0 +1,10 @@
+# COPYRIGHT © 2021-22 BY LEGENDX22
+from . import db
+x = db["PERSONAL_DATA"]
+async def add_info(data):
+  await x.insert_one({"data": data})
+async def remove_data(data):
+  await x.delete_one({"data": data})
+async def show_data():
+  X = [fuck for fuck in x.find({})]
+  return X
