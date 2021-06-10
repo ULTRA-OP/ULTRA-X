c1b5c22c794aa1a9ee12c3ba0d83f19c6ab2f19b
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-06-10 09:12:01 +0530

    Done

--- ULTRA/data/usersdata_db.py
+++ ULTRA/data/usersdata_db.py
@@ -2,9 +2,30 @@
 from . import db
 x = db["PERSONAL_DATA"]
 async def add_data(data):
-  await x.insert_one({"data": data})
-async def remove_data(data):
-  await x.delete_one({"data": data})
-async def show_data():
-  X = [fuck async for fuck in x.find()]
-  return X
+  k = await x.find_one({"_id": "LEGENDX"})
+  if k:
+    await x.update_one({"_id": "LEGENDX"}, {"$push": {"data": data}})
+  else:
+    data = [data]
+    await x.insert_one({"_id": "LEGENDX", "data": data})
+
+async def rm_data(data):
+  await x.update_one({"_id": "LEGENDX"}, {"$pull": {"data": data}})
+async def already_data(data):
+  pro = await x.find_one({"_id": "LEGENDX"})
+  if pro:
+    X = list(pro["data"])
+    if data in X:
+      return True
+    else:
+      return False
+  else:
+    return False
+
+async def all_data():
+  k = await x.find_one({'_id': "LEGENDX"})
+  if k:
+    return list(k["data"])
+  else:
+    return False
+    
