40aa828c76c18d5ce589122e55fab395f3eceeba
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-06-10 12:52:15 +0530

    Update users.py

--- ULTRA/plugins/users.py
+++ ULTRA/plugins/users.py
@@ -4,7 +4,7 @@
 
 ## COPY WITH CREDITS PLEASE
 
-from ..data.usersdb import add_data, already_data, rm_data, show_data
+from ..data.usersdata_db import add_data, already_data, rm_data, show_data
 GROUP_LIST = []
 GROUP_DICT = {}
 from .import *
