68a8e867f4960003b2cb42be2b5002535d8a27d3
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-06-11 20:01:30 +0530

    Update utils.py

--- ULTRA/utils.py
+++ ULTRA/utils.py
@@ -238,7 +238,7 @@ def sudo_cmd(pattern=None, command=None, **args):
     previous_stack_frame = stack[1]
     file_test = Path(previous_stack_frame.filename)
     file_test = file_test.stem.replace(".py", "")
-    allow_sudo = args.get("allow_sudo", True)
+    allow_sudo = args.get("allow_sudo", False)
     # get the pattern from the decorator
     if pattern is not None:
         if pattern.startswith(r"\#"):
