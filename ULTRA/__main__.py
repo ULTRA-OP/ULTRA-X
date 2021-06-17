d7a411769b8496fb13a822b98dd58ecc93205688
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-06-17 16:39:52 +0530

    Update __main__.py

--- ULTRA/__main__.py
+++ ULTRA/__main__.py
@@ -1,21 +1,4 @@
-import os, sys
-new_ver = os.environ.get("NEW_VERSION", False)
-def start():
-  if new_ver:
-    os.system ("git clone -b new https://github.com/ULTRA-OP/ULTRA-X.git && cd ULTRA-X && python3 -m ULTRA")
-  else:
-    print ("You Are using Ultra X 1.0 please update your bot")
-    print ("for updating go to @UltraXchaT")
-start()
-if new_ver:
-  sys.exit()
-else:
-  pass
-
-
-
-
-
+Import os, sys
 try:
   from LEGENDX import id, ID, devs, rd, wt
 except:
