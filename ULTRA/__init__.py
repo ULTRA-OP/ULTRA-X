70ca9362a38f808713116033617e02b3582db311
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-06-18 20:28:17 +0530

    Update __init__.py

--- ULTRA/__init__.py
+++ ULTRA/__init__.py
@@ -25,7 +25,7 @@ StartTime = time.time()
 os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
-    bot = UltraX(session_name, Var.APP_ID, Var.APP_HASH)
+    bot = UltraX(session_name, Var.APP_ID, Var.API_HASH)
 else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
