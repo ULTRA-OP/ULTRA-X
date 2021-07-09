b543ecad7387109fa2839a3cf99df74d0d51f0ab
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-09 08:59:22 +0530

    LoL

---
+++ ULTRA/plugins/secretpic.py
@@ -0,0 +1,18 @@
+# made by madboy482
+
+# made for ULTRA-X
+
+from .import *
+@bot.on(admin_cmd(pattern="secretpic"))
+async def oho(event):
+  if not event.is_reply:
+    return await event.edit('Reply to a self distructing pic !.!.!')
+  MADBOY = "😎🎮MสdBØy ✘😎"
+  MADBOI = f"tg://user?id={1732236209}"
+  k = await event.get_reply_message()
+  pic = await k.download_media()
+  await bot.send_file(event.chat_id, pic, caption=f"OwO!! LoL, Destruction Mode Pic Destroyed!!**\n__LoL, LmAo, SeD, RiP__\n**Sorry 🥺😂🤣**\n\n__Made By__ : **[{MADBOY}]({MADBOI})**)
+
+# made by madboy482
+
+# made for ULTRA-X
