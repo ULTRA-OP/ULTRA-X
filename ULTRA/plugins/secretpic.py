04fa9c06076471c13042aece16ae3ed8fd829c0c
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-07-09 11:46:29 +0530

    Update secretpic.py

--- ULTRA/plugins/secretpic.py
+++ ULTRA/plugins/secretpic.py
@@ -1,4 +1,5 @@
-# made by legendx22 and madboy482
+# COPYRIGHT (C) 2021-22 BY LEGENDX22
+# MADE BY LEGENDX AND MADBOY
 
 # made for ULTRA-X
 
@@ -7,15 +8,12 @@ from . import *
 async def oho(event):
   if not event.is_reply:
     return await event.edit('Reply to a self distructing pic !.!.!')
-  MADBOY = "😎🎮MสdBØy ✘😎"
-  MADBOI = f"tg://user?id={1732236209}"
-  LEGEND = "LEGEND X"
-  LEGENDX = f"tg://user?id={1667146381}"
   k = await event.get_reply_message()
   pic = await k.download_media()
   await bot.send_file(event.chat_id, pic, caption=f"""
-  OwO!! LoL, Destruction Mode Pic Destroyed!!**\n__LoL, LmAo, SeD, RiP__\n**Sorry 🥺😂🤣**\n\n__Made By__ : **[{MADBOY}]({MADBOI})**\n__Code By__ : **[{LEGEND}]({LEGENDX})**
-  """, parse_mode='md')
+  OwO!! LoL, Destruction Mode Pic Destroyed!!
+  Pic captured By UltraX
+  """)
   await event.delete()
   
 # made by legendx22 and madboy482
