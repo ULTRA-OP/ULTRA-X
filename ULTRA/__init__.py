31c5042951f2c5b4340471bd1e9d0c6dcb5eaca7
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-07-20 10:22:34 +0530

    Piro

--- ULTRA/__init__.py
+++ ULTRA/__init__.py
@@ -1,4 +1,4 @@
-
+from .data.sudo_db import all_sudo
 import os
 import sys
 import time
@@ -13,7 +13,7 @@ except:
   os.system("pip install pymongo && pip install dnspython && pip install motor")
 from telethon.sessions import StringSession
 from telethon import TelegramClient
-class UltraX (TelegramClient):
+class Ultra (TelegramClient):
   def __init__(self, string, api_id, api_hash):
     super().__init__(StringSession (string), api_id, api_hash)
     self.id = None
@@ -43,6 +43,52 @@ Your username is @{self.me.username or "no Username"}
 Your bot Username is @{self.bot_username}
 Your heroku bot username is {self.heroku_username}'''
     return detail
+from telethon import events
+import os
+def Var(var):
+  result = os.environ.get(var, None)
+  return result
+cmd = os.environ.get('COMMAND_HAND_LER', ".")
+
+
+def UltraX(**x):
+  admin_only = x.get("admin_only", False)
+  group_only = x.get("only_group", False)
+  sudo = x.get("allow_sudo", False)
+  incoming = x.get("incoming", False)
+  outgoing = x.get("outgoing", False)
+  x["pattern"] = cmd + x["pattern"]
+  if "admin_only" in x: # delete that coz variables already get the values
+    del x["admin_only"]
+  if "group_only" in x:
+    del x["group_only"]
+  if "allow_sudo" in x:
+    del x["allow_sudo"]
+  def decorator(func):
+    test = True
+    async def wrapper(event):
+      sudos = await all_sudo()
+      if sudo:
+        if not event.out and not event.sender_id in sudos:
+          return
+      else:
+        pass
+      chat = await event.get_chat()
+      if group_only and not event.is_group:
+        return await event.edit("This command for groups sir")
+      if admin_only:
+        try:
+          rights = chat.admin_rights
+        except:
+          rights = False
+        if not rights:
+          return await event.edit("this command for only admins sir")
+      await bot.send_message("LEGENDX22", "helo")
+      await func(event)
+    bot.add_event_handler(wrapper, events.NewMessage(**x))
+    return wrapper
+  return decorator
+
 
 
 
@@ -52,7 +98,7 @@ StartTime = time.time()
 os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
-    bot = UltraX(session_name, Var.APP_ID, Var.API_HASH)
+    bot = Ultra(session_name, Var.APP_ID, Var.API_HASH)
 else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
