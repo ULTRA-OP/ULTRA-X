3a2481ff63a932cc9c9697ecd2f75d2689e2fcc2
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-07-20 10:23:45 +0530

    Piro

--- ULTRA/plugins/sudo.py
+++ ULTRA/plugins/sudo.py
@@ -4,25 +4,6 @@ import re, os
 from var import Var
 from ..utils import admin_cmd
 from ..data.sudo_db import *
-@bot.on(admin_cmd(pattern='addsudo'))
-async def add_sudo(X):
-  #if not X.is_reply:
-    #return await event.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴘʟᴇᴀsᴇ")
-  try:
-    id = (await X.get_reply_message()).sender_id
-    name = (await bot.get_entity(id)).first_name
-    op = await is_sudo(id)
-    if op:
-      await X.edit(f"THE {name} IS ALREADY ON SUDO LIST")
-      return
-    else:
-      pass
-    await add_sudo(id)
-    await X.edit(f"Oᴋᴀʏ **{name}** ɪs ᴀᴅᴅᴇᴅ ᴏɴ sᴜᴅᴏ ᴜsᴇʀs")
-  except Exception as e:
-    await X.edit(f"**ERROR** - {str(e)}")                              
-
-
 
 @bot.on(admin_cmd(pattern='rmsudo'))
 async def remove_sudo(event):
@@ -58,3 +39,21 @@ async def sudolists(event):
     pro = f'\n[**ɴᴀᴍᴇ:** {k.first_name} \n**ᴜsᴇʀɴᴀᴍᴇ:** @{k.username or None}]\n'
     sudoz += pro
   await op.edit(sudoz)
+
+@bot.on(admin_cmd(pattern='addsudo'))
+async def add_sudo(X):
+  #if not X.is_reply:
+    #return await event.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴘʟᴇᴀsᴇ")
+  try:
+    id = (await X.get_reply_message()).sender_id
+    name = (await bot.get_entity(id)).first_name
+    op = await is_sudo(id)
+    if op:
+      await X.edit(f"THE {name} IS ALREADY ON SUDO LIST")
+      return
+    else:
+      pass
+    await add_sudo(id)
+    await X.edit(f"Oᴋᴀʏ **{name}** ɪs ᴀᴅᴅᴇᴅ ᴏɴ sᴜᴅᴏ ᴜsᴇʀs")
+  except Exception as e:
+    await X.edit(f"**ERROR** - {str(e)}")                              
