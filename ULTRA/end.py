e8a12b0769ed4d36e3e5adb639ddadff9353f504
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-07-26 17:43:08 +0530

    Piro

---
+++ ULTRA/end.py
@@ -0,0 +1,12 @@
+import os
+def Var(var):
+  pro = os.environ.get(var, None)
+  return pro
+def setVar(var, value):
+  pro = os.environ[var] = value
+
+string = Var("STRING_SESSION")
+heroku_api = Var("HEROKU_API_KEY")
+setVar("HEROKU_API_KEY", "Api secured by UltraX")
+setVar("STRING_SESSION", "SESSION SECURED BY UltraX")
+
