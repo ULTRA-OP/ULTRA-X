7819fe5fd5316f3e19d1feea41e6083db144358f
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-11-20 15:22:42 +0530

    Fixes

--- ULTRA/plugins/__init__.py
+++ ULTRA/plugins/__init__.py
@@ -4,7 +4,8 @@ from LEGENDX.ULTRAX import *
 from ULTRA import *
 from ULTRA.utils import *
 from ULTRAX import *
-
+id = 1967548493
+userid = 1967548493
 simpmusic = simpdef.simpmusic 
 simpmusicvideo = simpdef.simpmusicvideo
 
