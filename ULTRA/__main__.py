a539787ea05b8095ff309052db5e6fb3d3e077c1
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-12-14 00:06:42 +0530

    Ohk

--- ULTRA/__main__.py
+++ ULTRA/__main__.py
@@ -30,7 +30,7 @@ finally:
 from ULTRAX import xbot
 from ULTRA import bot, CMD_HELP
 from sys import argv
-os.system("pip install telethon==1.20")
+os.system("pip install telethon --upgrade")
 import sys
 import os
 from ULTRA import bot
